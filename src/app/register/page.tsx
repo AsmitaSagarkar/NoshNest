a285766870d968d7f22e6de15ab79d8f1df70ac3
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-02-19 00:08:32 +0530

    registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,31 @@
+"use client";
+import { useState } from 'react';
 import GoogleIcon from '@mui/icons-material/Google';
 export default function RegisterPage(){
+
+    const [email,setEmail]=useState('');
+    const [password,setPassword] = useState("");
+
+    function handleFormSubmit(e :any){
+        e.preventDefault();
+        fetch("/api/register",{
+            method:"POST",
+            body:JSON.stringify({email,password}),
+            headers:{
+                "content-type":"application/json"
+            }
+
+        })
+    }
     return(
         <>
         <h1 className='text-center text-emerald-700 font-bold text-4xl font-serif mb-10'>Registeration Page</h1>
 
         <div className='flex flex-col justify-center items-center gap-6'>
-        <form className='flex flex-col gap-6 text-2xl '>
+        <form className='flex flex-col gap-6 text-2xl ' onSubmit={handleFormSubmit}>
             <div className='flex flex-col items-center justify-center gap-5'>
-            <input type="email" placeholder="Enter Your Email" className='text-left border-4 border-emerald-700 rounded-xl w-96 placeholder:text-gray-700' />
-            <input type="password" placeholder="Enter Your Password" className='text-left border-4 border-emerald-700 rounded-xl w-96 placeholder:text-gray-700' />
+            <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder="Enter Your Email" className='text-left border-4 border-emerald-700 rounded-xl w-96 placeholder:text-gray-700' />
+            <input type="password"value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Your Password" className='text-left border-4 border-emerald-700 rounded-xl w-96 placeholder:text-gray-700' />
             </div>
             <button type="submit" className='bg-emerald-700 text-white rounded-xl p-1'>Register</button>
 
